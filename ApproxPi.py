8b53ba69db2f342b2988c7a7d8f822789f820e3d
lulee13 <lucaslee@unomaha.edu>
2025-02-05 15:29:46 +0000

    Temperate

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,9 +12,13 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-
+  precision = input("How many decimal places would you like? ")
+  
   start = time.time()
   #calculate pi using the approximation technique
+  
+
+
   #Loop until the level of percision is reached
 
   end = time.time()
--- TempConvert.py
+++ TempConvert.py
@@ -6,12 +6,15 @@
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  fTemp = input ("Input a temperature in degrees Fahrenheit: ")
+
   #Convert that temperature to celsius, rounding to 1 decimal percision
+  cTemp = (float(fTemp) - 32) * (5/9)
+  cTempRound = round (cTemp, 1)
+
   #Output converted temperature.
-  tempF = 80
+  
 
-  tempC = tempF / 2
-
-  print(tempF, "is ", tempC, "degrees celsius.")
+  print(fTemp, "degrees Fahrenheit is", cTempRound, "degrees Celsius.")
 if __name__ == '__main__':
   main()
